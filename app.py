6daa444c62453f5cb33af442404ff9efd2931f16
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-08-23 12:06:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,90 +3,21 @@ import os
 import time
 import glob
 import os
-
-
 from gtts import gTTS
-from googletrans import Translator
+
 
 try:
     os.mkdir("temp")
 except:
     pass
 st.title("Text to speech")
-translator = Translator()
 
 text = st.text_input("Enter text")
-in_lang = st.selectbox(
-    "Select your input language",
-    ("English", "Spanish", "Bengali", "korean", "Chinese", "Japanese"),
-)
-if in_lang == "English":
-    input_language = "en"
-elif in_lang == "Spanish":
-    input_language = "es"
-elif in_lang == "Bengali":
-    input_language = "bn"
-elif in_lang == "korean":
-    input_language = "ko"
-elif in_lang == "Chinese":
-    input_language = "zh-cn"
-elif in_lang == "Japanese":
-    input_language = "ja"
-
-out_lang = st.selectbox(
-    "Select your output language",
-    ("English", "Spanish", "Bengali", "korean", "Chinese", "Japanese"),
-)
-if out_lang == "English":
-    output_language = "en"
-elif out_lang == "Spanish":
-    output_language = "es"
-elif out_lang == "Bengali":
-    output_language = "bn"
-elif out_lang == "korean":
-    output_language = "ko"
-elif out_lang == "Chinese":
-    output_language = "zh-cn"
-elif out_lang == "Japanese":
-    output_language = "ja"
-
-english_accent = st.selectbox(
-    "Select your english accent",
-    (
-        "Default",
-        "India",
-        "United Kingdom",
-        "United States",
-        "Canada",
-        "Australia",
-        "Ireland",
-        "South Africa",
-    ),
-)
-
-if english_accent == "Default":
-    tld = "com"
-elif english_accent == "India":
-    tld = "co.in"
-
-elif english_accent == "United Kingdom":
-    tld = "co.uk"
-elif english_accent == "United States":
-    tld = "com"
-elif english_accent == "Canada":
-    tld = "ca"
-elif english_accent == "Australia":
-    tld = "com.au"
-elif english_accent == "Ireland":
-    tld = "ie"
-elif english_accent == "South Africa":
-    tld = "co.za"
 
 
-def text_to_speech(input_language, output_language, text, tld):
-    translation = translator.translate(text, src=input_language, dest=output_language)
-    trans_text = translation.text
-    tts = gTTS(trans_text, lang=output_language, tld=tld, slow=False)
+def text_to_speech(text, tld):
+    
+    tts = gTTS(text,"es", tld=tld, slow=False)
     try:
         my_file_name = text[0:20]
     except:
@@ -98,7 +29,7 @@ def text_to_speech(input_language, output_language, text, tld):
 display_output_text = st.checkbox("Display output text")
 
 if st.button("convert"):
-    result, output_text = text_to_speech(input_language, output_language, text, tld)
+    result, output_text = text_to_speech("es", "es", text, tld)
     audio_file = open(f"temp/{result}.mp3", "rb")
     audio_bytes = audio_file.read()
     st.markdown(f"## Your audio:")
