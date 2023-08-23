038614ae1a1adefe7a0880572fecc8b397d3cab7
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-08-23 14:27:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 from gtts import gTTS
 from PIL import Image
 
-st.title("Interfaz Multimodal 1.")
+st.title("Interfases Multimodales.")
 image = Image.open('text_to_audio.png')
 
 st.image(image, width=200)
