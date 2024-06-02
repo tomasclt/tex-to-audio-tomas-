ed4a9d5385960d071849382862a8a34d5138fc97
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-06-01 19:54:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ st.subheader("Texto a audio.")
 st.write('¡Ay! -dijo el ratón-. El mundo se hace cada día más pequeño. Al principio era tan grande que le tenía miedo. '  
          ' Corría y corría y por cierto que me alegraba ver esos muros, a diestra y siniestra, en la distancia. ' 
          ' Pero esas paredes se estrechan tan rápido que me encuentro en el último cuarto y ahí en el rincón está '  
-         ' la trampa sobre la cual debo pasar. Todo lo que debes hacer es cambiar de rumbo -dijo el gato...y se lo comió. ' 
-         '/n'
+         ' la trampa sobre la cual debo pasar. Todo lo que debes hacer es cambiar de rumbo dijo el gato...y se lo comió. ' 
+         '\n'
          ' Franz Kafka.'
         
         )
