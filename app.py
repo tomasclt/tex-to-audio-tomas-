70976e7c30826c660aacc6b9edad5467cdd8aa12
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-06-01 20:09:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def text_to_speech(text, tld):
 
 #display_output_text = st.checkbox("Verifica el texto")
 
-if st.button("convertir"):
+if st.button("convertir a Audio"):
      result, output_text = text_to_speech(text, tld)
      audio_file = open(f"temp/{result}.mp3", "rb")
      audio_bytes = audio_file.read()
